0d76da2c3198741deb462bce26fedb9a4160dcd1
aura-herrera-sonarsource <aura.herrera@sonarsource.com>
2023-04-24 12:45:26 +0100

    Initial code

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "random" {}
+
+variable "first_last_name" {
+  default = {
+    "Jan" : "Janssen",
+    "Kees" : "Bakker"
+  }
+}
+
+module "name_complete" {
+  for_each   = var.first_last_name
+  source     = "./module_name/"
+  first_name = each.key
+  last_name  = each.value
+}
+
+data "http" "myip" {
+  url = "https://api.ipify.org"
+}
+
+resource "null_resource" "test" {
+  count = 5
+}
+
